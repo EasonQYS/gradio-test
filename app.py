a3cb6fc197c9d769d5673e7a0e3ffbbc82129a7d
EasonQYS <easonqys@foxmail.com>
2024-08-07 14:40:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ import os
 import shutil
 print(os.system('ls -la'))
 print('***********')
-print(os.system('ls -la '))
+print(os.system('pwd'))
+
+def do2(fileobj):
+    print(fileobj.name)
+    path = "./" + os.path.basename(fileobj.name)  #NB*
+    shutil.copyfile(fileobj.name, path)
+    return path
 
 with gr.Blocks() as demo:
     file_path = gr.FileExplorer(root_dir='./')
+    with gr.Row():
+        f1 = gr.File()
+        b2 = gr.Button("语音转文本")
+        f2 = gr.File()
+    b2.click(do2, inputs=f1,outputs=f2)
 
 demo.launch()
 
