7ec7f889661fe2863cf85e7425db20fdabc54afd
EasonQYS <easonqys@foxmail.com>
2024-08-13 20:54:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os,random
 
 num = random.randint(1000,9999)
-base_path = './model'+str(num)
+base_path = './model'#+str(num)
 
 if os.path.exists(base_path):
     print("文件已存在:", base_path)
     os.system(f'rm -rf {base_path}')
-os.system('git lfs install')
+os.system('git lfs install --force')
 os.system(f'git clone https://code.openxlab.org.cn/yisheng/snh_pocket48.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
